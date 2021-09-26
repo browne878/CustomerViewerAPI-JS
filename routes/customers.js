68ac1da50dc1e253809ca3784f53bdaf04087bb9
browne878 <78582151+browne878@users.noreply.github.com>
2021-09-26 13:35:16 +0100

    Add error check to Get single customer

--- routes/customers.js
+++ routes/customers.js
@@ -17,7 +17,7 @@ router.get('/:id', (req, res) => {
     db.GetCustomer(req.params.id).then(result => {
         console.log(result);
         res.send(result);
-    })
+    });
 });
 
 router.post('/', (req, res) => {
